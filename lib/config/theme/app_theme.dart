11291090487ed6ac82179aeef2d3a76c16564e2f
Claudia Cadec <claudiacadec@outlook.com>
2023-04-27 21:31:21 +0200

    commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+const Color _customColor = Colors.deepPurpleAccent;
+const List < Color> _colorThemes = [];
+
+class AppTheme {
+  ThemeData theme() {
+    return ThemeData(
+      useMaterial3: true
+    );
+  }
+}
