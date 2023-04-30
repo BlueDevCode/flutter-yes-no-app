5135ff745286733916cc92e98a674324fb10aab5
Claudia Cadec <claudiacadec@outlook.com>
2023-04-30 20:20:41 +0200

    commit

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -7,14 +7,34 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading:  Padding(
-          padding: const EdgeInsets.all(4.0),
+        leading:  const Padding(
+          padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
             backgroundImage: NetworkImage("https://pngimg.com/uploads/brad_pitt/brad_pitt_PNG17.png"),
           ),
         ),
         title: const Text("My love ❤️"),
         centerTitle: false,
+      ),
+      body: const _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+      children: [
+        Expanded(child: Container(color: Colors.red,)),
+        Text("Hola")
+      ],
+    
       ),
     );
   }
