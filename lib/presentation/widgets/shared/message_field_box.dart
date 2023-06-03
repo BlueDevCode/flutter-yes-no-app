378bbc3076b5b2679a75130c6395cfdf80850a33
Claudia Cadec <claudiacadec@outlook.com>
2023-06-03 18:54:09 +0200

    commit

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,11 +5,29 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  TextFormField(
-      decoration: InputDecoration(
-        filled: true,
-        suffixIcon: Icon(Icons.send_outlined)
-      )
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color:Colors.transparent),
+      borderRadius: BorderRadius.circular(40)
     );
+    final inputDecoration = InputDecoration(
+            enabledBorder: outlineInputBorder,
+            focusedBorder: outlineInputBorder,
+            filled: true,
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.send_outlined),
+              onPressed: () {
+                print("valor de la caja de texto");
+              },
+            ));
+    return TextFormField(
+        decoration: inputDecoration,
+        onFieldSubmitted: (value) {
+          print("Summit value $value");
+        },
+        onChanged:(value) {
+          print("changed : $value");
+        }, 
+        );
   }
 }
